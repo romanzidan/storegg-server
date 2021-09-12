198b1c2dfcb761ae1da654244dd3110db0d12f4f
rzkzx <romanzidanramadhan@gmail.com>
2021-09-12 22:58:17 +0800

    Nominal CRUD - Read

--- app/nominal/router.js
+++ app/nominal/router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const {index, viewCreate, actionCreate} = require('./controller');
 
-router.get('/', index);
+router.get('/', index)
 router.get('/create', viewCreate)
 router.post('/create', actionCreate)
 
