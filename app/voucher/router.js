15912c85dbffc1fe0a4d2dbe8eb4031c7cee9229
rzkzx <romanzidanramadhan@gmail.com>
2021-09-13 17:31:48 +0800

    Voucher - Update Status

--- app/voucher/router.js
+++ app/voucher/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const {
-  index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete
+  index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete, actionStatus
 } = require('./controller')
 const multer = require('multer')
 const os = require('os')
@@ -12,5 +12,6 @@ router.post('/create', multer({dest: os.tmpdir()}).single('image'), actionCreate
 router.get('/edit/:id', viewEdit)
 router.put('/edit/:id', multer({dest: os.tmpdir()}).single('image'), actionEdit)
 router.delete('/delete/:id', actionDelete)
+router.put('/status/:id', actionStatus)
 
 module.exports = router;
