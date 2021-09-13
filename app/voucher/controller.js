13b6855ec07c3fccfe83b07d0a9db4d9cdf84fbe
rzkzx <romanzidanramadhan@gmail.com>
2021-09-13 13:28:28 +0800

    Voucher CRUD - Read

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -13,6 +13,8 @@ module.exports = {
 
       const alert = { message: alertMessage, status: alertStatus }
       const voucher = await Voucher.find()
+      .populate('category')
+      .populate('nominals')
 
       res.render('admin/voucher/view_voucher',{
         voucher,
