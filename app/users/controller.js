18170c559082560a89b1769995ef911daf7ea676
rzkzx <romanzidanramadhan@gmail.com>
2021-09-14 13:30:19 +0800

    Admin Sign In Page

---
+++ app/users/controller.js
@@ -0,0 +1,99 @@
+const User = require('./model')
+
+module.exports = {
+  index: async(req,res)=>{
+    try{
+      const alertMessage = req.flash('alertMessage')
+      const alertStatus = req.flash('alertStatus')
+
+      res.render('admin/users/view_signin',{
+        title: 'Nominal'
+      })
+    }catch(err){
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/user')
+    }
+  },
+  // viewCreate: async(req,res)=>{
+  //   try {
+  //     res.render('admin/nominal/create',{
+  //       title: 'Create Nominal'
+  //     })
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/nominal')
+  //   }
+  // },
+  // actionCreate: async(req,res)=>{
+  //   try {
+  //     const {coinName, coinQuantity, price} = req.body;
+
+  //     let nominal = await Nominal({coinName, coinQuantity, price})
+  //     await nominal.save();
+
+  //     req.flash('alertMessage', 'Create nominal successfully')
+  //     req.flash('alertStatus', 'success')
+
+  //     res.redirect('/nominal')
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/nominal')
+  //   }
+  // },
+  // viewEdit: async(req,res)=>{
+  //   try {
+  //     const {id} = req.params
+
+  //     const nominal = await Nominal.findOne({_id: id})
+
+  //     res.render('admin/nominal/edit',{
+  //       title: 'Edit Nominal',
+  //       nominal
+  //     })
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/nominal')
+  //   }
+  // },
+  // actionEdit: async(req,res)=>{
+  //   try {
+  //     const { id } = req.params;
+  //     const { coinName, coinQuantity, price } = req.body;
+
+  //     await Nominal.findOneAndUpdate({
+  //       _id: id
+  //     },{
+  //       coinName, coinQuantity, price
+  //     })
+
+  //     req.flash('alertMessage', 'Edit nominal successfully')
+  //     req.flash('alertStatus', 'success')
+
+  //     res.redirect('/nominal')
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/nominal')
+  //   }
+  // },
+  // actionDelete: async(req,res)=>{
+  //   try {
+  //     const { id } = req.params;
+
+  //     await Nominal.findOneAndRemove({_id:id})
+
+  //     req.flash('alertMessage', 'Delete nominal successfully')
+  //     req.flash('alertStatus', 'success')
+
+  //     res.redirect('/nominal')
+  //   } catch (err) {
+  //     req.flash('alertMessage', `${err.message}`)
+  //     req.flash('alertStatus', 'danger')
+  //     res.redirect('/nominal')
+  //   }
+  // }
+}
---
+++ app/users/model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+let userSchema = mongoose.Schema({
+  
+})
+
+module.exports = mongoose.model('User', userSchema)
