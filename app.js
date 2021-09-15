df2bf56a8812919d14fe9bd3275076ed879c4e94
rzkzx <romanzidanramadhan@gmail.com>
2021-09-15 20:35:33 +0800

    API - Landing Page

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 
-var dashboardRouter = require('./app/dashboard/router');
-var categoryRouter = require('./app/category/router');
-var nominalRouter = require('./app/nominal/router');
-var voucherRouter = require('./app/voucher/router');
-var bankRouter = require('./app/bank/router');
-var paymentRouter = require('./app/payment/router');
-var usersRouter = require('./app/users/router');
-var transactionRouter = require('./app/transaction/router');
+const dashboardRouter = require('./app/dashboard/router');
+const categoryRouter = require('./app/category/router');
+const nominalRouter = require('./app/nominal/router');
+const voucherRouter = require('./app/voucher/router');
+const bankRouter = require('./app/bank/router');
+const paymentRouter = require('./app/payment/router');
+const usersRouter = require('./app/users/router');
+const transactionRouter = require('./app/transaction/router');
+const playerRouter = require('./app/player/router');
 
-var app = express();
+const app = express();
+const URL = `/api/v1`;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,6 +47,9 @@ app.use('/voucher', voucherRouter);
 app.use('/bank', bankRouter);
 app.use('/payment', paymentRouter);
 app.use('/transaction', transactionRouter);
+
+// api
+app.use(`${URL}/players`, playerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app/player/router.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const {
+  landingPage
+} = require('./controller')
+
+router.get('/landingpage', landingPage)
+
+module.exports = router;
