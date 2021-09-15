90bb3fae0116bc2f6ebbc0e1c0322598d061d151
rzkzx <romanzidanramadhan@gmail.com>
2021-09-15 12:07:04 +0800

    Transaction Status Update

--- app/transaction/router.js
+++ app/transaction/router.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const {
-  index
+  index, actionStatus
 } = require('./controller')
 const {isLoginAdmin} = require('../middleware/auth')
 
 router.use(isLoginAdmin);
 router.get('/', index)
+router.put('/status/:id', actionStatus)
 
 module.exports = router;
