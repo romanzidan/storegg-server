f8c1f6c8c07c42bd7b853a6752713de5bf569ace
rzkzx <romanzidanramadhan@gmail.com>
2021-09-16 07:30:10 +0800

    API - Sign In with JWT

--- app/auth/router.js
+++ app/auth/router.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const {
-  signup
+  signup, signin
 } = require('./controller')
 const multer = require('multer')
 const os = require('os')
 
 router.post('/signup', multer({dest: os.tmpdir()}).single('avatar') ,signup)
+router.post('/signin', signin)
 
 module.exports = router;
