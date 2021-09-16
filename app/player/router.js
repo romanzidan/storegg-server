3168847ff1f1ad31990fc57381e6e71ce285d986
rzkzx <romanzidanramadhan@gmail.com>
2021-09-16 19:49:54 +0800

    API - Checkout

--- app/player/router.js
+++ app/player/router.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const {
-  landingPage, detailPage, category
+  landingPage, detailPage, category, checkout
 } = require('./controller')
+const { isLoginPlayer } = require('../middleware/auth')
 
 router.get('/landingpage', landingPage)
 router.get('/:id/detail', detailPage)
 router.get('/category', category)
+router.post('/checkout', isLoginPlayer, checkout)
 
 module.exports = router;
