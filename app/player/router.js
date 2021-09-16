f027acec689fed56fb419e1d0fac3e78562642ef
rzkzx <romanzidanramadhan@gmail.com>
2021-09-17 07:49:54 +0800

    API - History Transactions

--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const {
-  landingPage, detailPage, category, checkout
+  landingPage, detailPage, category, checkout, history
 } = require('./controller')
 const { isLoginPlayer } = require('../middleware/auth')
 
@@ -9,5 +9,6 @@ router.get('/landingpage', landingPage)
 router.get('/:id/detail', detailPage)
 router.get('/category', category)
 router.post('/checkout', isLoginPlayer, checkout)
+router.get('/history', isLoginPlayer, history)
 
 module.exports = router;
