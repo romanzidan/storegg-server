191087f4d818c5b0faaad3b16a7155fe40d3e565
rzkzx <romanzidanramadhan@gmail.com>
2021-09-17 08:14:12 +0800

    API - Dashboard

--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const {
-  landingPage, detailPage, category, checkout, history, historyDetail
+  landingPage, detailPage, category, checkout, history, historyDetail, dashboard
 } = require('./controller')
 const { isLoginPlayer } = require('../middleware/auth')
 
@@ -11,5 +11,6 @@ router.get('/category', category)
 router.post('/checkout', isLoginPlayer, checkout)
 router.get('/history', isLoginPlayer, history)
 router.get('/history/:id/detail', isLoginPlayer, historyDetail)
+router.get('/dashboard', isLoginPlayer, dashboard)
 
 module.exports = router;
