fd7a62dd2cbc3f8589ed5a153c8366f816c53e04
rzkzx <romanzidanramadhan@gmail.com>
2021-09-17 09:12:42 +0800

    API - Update Profile

--- app/player/controller.js
+++ app/player/controller.js
@@ -5,6 +5,9 @@ const Nominal = require('../nominal/model')
 const Payment = require('../payment/model')
 const Bank = require('../bank/model')
 const Transaction = require('../transaction/model')
+const path = require('path')
+const fs = require('fs')
+const config = require('../../config')
 
 module.exports = {
   landingPage: async(req,res)=>{
@@ -227,6 +230,75 @@ module.exports = {
       })
     } catch (err) {
       res.status(500).json({message: err.message || `Internal Server Error`})
+    }
+  },
+
+  editProfile: async(req, res)=>{
+    try {
+      const {name = '', phoneNumber = ''} = req.body
+
+      const payload = {}
+
+      if(name.length) payload.name = name
+      if(phoneNumber.length) payload.phoneNumber = phoneNumber
+
+      if(req.file){
+        let tmp_path = req.file.path;
+        let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+        let filename = req.file.filename + '.' + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
+
+        const src = fs.createReadStream(tmp_path)
+        const dest = fs.createWriteStream(target_path)
+
+        src.pipe(dest)
+
+        src.on('end', async()=>{
+            let player = await Player.findOne({_id: req.player._id})
+
+            let currentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
+            if(fs.existsSync(currentImage)){
+              fs.unlinkSync(currentImage)
+            }
+
+            player = await Player.findOneAndUpdate({
+              _id : req.player._id
+            },{
+              ...payload,
+              avatar: filename
+            }, {new: true, runValidators: true})
+
+            res.status(201).json({
+              data: {
+                id: player._id,
+                name: player.name,
+                phoneNumber: player.phoneNumber,
+                avatar: player.avatar
+              }
+            })
+        })
+      }else{
+        const player = await Player.findOneAndUpdate({
+          _id: req.player._id
+        }, payload, {new: true, runValidators: true})
+
+        res.status(201).json({
+          data: {
+            id: player._id,
+            name: player.name,
+            phoneNumber: player.phoneNumber,
+            avatar: player.avatar
+          }
+        })
+      }
+    } catch (err) {
+      if(err && err.name === "ValidationError"){
+        res.status(422).json({
+          error: 1,
+          message: err.message,
+          field: err.errors
+        })
+      }
     }
   }
 }
--- app/player/router.js
+++ app/player/router.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const {
-  landingPage, detailPage, category, checkout, history, historyDetail, dashboard, profile
+  landingPage, detailPage, category, checkout, history, historyDetail, dashboard, profile, editProfile
 } = require('./controller')
 const { isLoginPlayer } = require('../middleware/auth')
+const multer = require('multer')
+const os = require('os')
 
 router.get('/landingpage', landingPage)
 router.get('/:id/detail', detailPage)
@@ -13,5 +15,6 @@ router.get('/history', isLoginPlayer, history)
 router.get('/history/:id/detail', isLoginPlayer, historyDetail)
 router.get('/dashboard', isLoginPlayer, dashboard)
 router.get('/profile', isLoginPlayer, profile)
+router.put('/profile', isLoginPlayer, multer({dest: os.tmpdir()}).single('avatar'), editProfile)
 
 module.exports = router;
